0d666a91b953804484cd703f681c80561563d12d
harroshan <harroshan@users.noreply.github.com>
2020-08-20 20:26:41 +0530

    JPA Initiated

--- src/main/java/com/harroshan/recipe/controller/IndexController.java
+++ src/main/java/com/harroshan/recipe/controller/IndexController.java
@@ -1,4 +1,16 @@
 package com.harroshan.recipe.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexController {
+
+    @RequestMapping({"", "/", "index"})
+    public String getIndexPage(Model model) {
+        String temp = "Hello Index Page";
+        model.addAttribute("temp", temp);
+        return "index";
+    }
 }
